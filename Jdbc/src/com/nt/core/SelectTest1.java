149f40aaecc40595db690f24c0a8cb7942e5c695
arvind01A <arvind.kumar208414@gmail.com>
2024-01-31 15:39:41 +0530

    initial update

---
+++ Jdbc/src/com/nt/core/SelectTest1.java
@@ -0,0 +1,32 @@
+package com.nt.core;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class SelectTest1 {
+	public static void main(String[] args) throws Exception{
+		//Load JDBC driver
+		Class.forName("oracle.jdbc.driver.OracleDriver");
+		
+		//Connetion establish
+		Connection con=DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl","mydb9am","arvind");
+		
+		//Create Statement object
+		Statement st = con.createStatement();
+		
+		//Send and execute the SELECT SQL Query to DB s/w
+		ResultSet rs = st.executeQuery("SELECT * FROM STUDENT");
+		
+		//process the ResultSet object
+		while(rs.next()!=false) {
+			System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getInt(3));
+		}//while
+		
+		//close the JDBC objects
+		con.close();
+		st.close();
+		rs.close();
+	}//main
+}//class
