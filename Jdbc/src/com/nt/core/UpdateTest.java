5b3e6eaf9018b1c9ec96d231d505e30fcd102b7a
arvind01A <arvind.kumar208414@gmail.com>
2024-02-01 15:59:34 +0530

    Non select update query perform

---
+++ Jdbc/src/com/nt/core/UpdateTest.java
@@ -0,0 +1,54 @@
+//UpdateTest.java
+package com.nt.core;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
+
+public class UpdateTest {
+	public static void main(String[] args) {
+		try(Scanner sc = new Scanner(System.in)){
+			try(Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl","mydb9am","arvind")){
+				try(Statement st = con.createStatement()){
+					String newAddrs = null;
+					float newAvg = 0.0f;
+					int no = 0;
+					
+					System.out.print("Enter student number:: ");
+					no = sc.nextInt();
+					System.out.print("Enter new Address for the student:: ");
+					newAddrs = sc.next();
+					System.out.println("Enter new average for the student:: ");
+					newAvg = sc.nextFloat();
+					
+					//convert the input value required for the SQL Query
+					newAddrs = "'"+newAddrs+"'";
+					
+					//prepare the SQL Query
+					String query = "UPDATE STUDENT_TEST SET SADD=" + newAddrs + ", AVG=" + newAvg + " WHERE STID=" + no;
+					System.out.println(query);
+					
+					//send and executeUpdate
+					int count = 0;
+					try(ResultSet rs = st.executeQuery(query)){
+						if(rs!=null)
+							count++;
+						if(count==0)
+							System.out.println("Record not found");
+						else
+							System.out.println(count + " no of record is found and update");
+					}//try4
+				}//try3
+			}//try2
+		}//try1
+		catch(SQLException se) {
+			se.printStackTrace();
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+		}
+	}//main
+}//class
